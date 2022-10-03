9b81cb022576b9688d1b75b3f60aefc7dc5d94f9
TomSa99 <95915840+TomSa99@users.noreply.github.com>
2022-10-03 16:16:03 +0100

    Update data_file.py

--- src/si/data/io/data_file.py
+++ src/si/data/io/data_file.py
@@ -13,4 +13,19 @@ def read_data_file (filename, sep =',',label=False):
         return Dataset(x, y, label=True)
     else:
         data = genfromtxt(filename, delimiter=sep)
-        return Dataset(data)
+        return Dataset(data, label=False)
+
+def write_data_file (dataset, filename, sep =',', label=False):
+    '''Writes a Dataset object into a .txt file'''
+    if label:
+        x = dataset.x
+        y = dataset.y
+        data = np.concatenate((x, y), axis=1)
+        savetxt(filename, data, delimiter=sep)
+    else:
+        savetxt(filename, dataset.x, delimiter=sep)
+
+# test
+if __name__ == '__main__':
+    dataset = read_data_file('data.csv', sep =',', label=True)
+    write_data_file(dataset, 'data.txt', sep =',', label=True)
