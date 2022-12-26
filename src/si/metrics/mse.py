6d4c84fd9e2aaa304259e37aa18bb07c6fed116c
TomSa99 <95915840+TomSa99@users.noreply.github.com>
2022-12-26 18:13:58 +0000

    [FIX] mse

--- src/si/metrics/mse.py
+++ src/si/metrics/mse.py
@@ -18,3 +18,21 @@ def mse(y_true: np.ndarray, y_pred: np.ndarray) -> float:
         The mean squared error of the model
     """
     return np.sum((y_true - y_pred) ** 2) / (len(y_true) * 2)
+
+def mse_derivative(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    """
+    It returns the derivative of the mean squared error of the model on the given dataset
+
+    Parameters
+    ----------
+    y_true: np.ndarray
+        The true labels of the dataset
+    y_pred: np.ndarray
+        The predicted labels of the dataset
+
+    Returns
+    -------
+    mse_derivative: np.ndarray
+        The derivative of the mean squared error of the model
+    """
+    return -2 * (y_true - y_pred) / (len(y_true) * 2)
